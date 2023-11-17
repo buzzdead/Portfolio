14994807390f978b0a2aa58d1ee04b7f5fbf2b8c
buzzdead <sigmundvolden@gmail.com>
2023-11-17 01:59:13 +0100

    changed to button instead

--- src/components/webcam.tsx
+++ src/components/webcam.tsx
@@ -1,32 +1,43 @@
-import React, { useRef, useEffect, useCallback } from 'react';
-import Webcam from "react-webcam";
+import { Button, Flex } from '@chakra-ui/react'
+import React, { useRef, useCallback } from 'react'
+import Webcam from 'react-webcam'
+const image = require('../../public/camerabutton.jpg')
+
 
 interface Props {
-    onCapture: (image: string | null) => void;
-    shouldCapture: boolean;
+  onCapture: (image: string | null) => void
+  shouldCapture: boolean
 }
 
 const WebcamCapture: React.FC<Props> = ({ onCapture, shouldCapture }) => {
-    const webcamRef = useRef<Webcam>(null);
+  const webcamRef = useRef<Webcam>(null)
+  console.log(shouldCapture, "in the comp")
 
-    const capture = useCallback(() => {
-        if (shouldCapture) {
-            const image = webcamRef.current?.getScreenshot();
-            onCapture(image || null);
-        }
-    }, [webcamRef, shouldCapture, onCapture]);
+  const capture = useCallback(() => {
+    if (shouldCapture) {
+      const image = webcamRef.current?.getScreenshot()
+      onCapture(image || null)
+    }
+  }, [webcamRef, shouldCapture, onCapture])
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            capture();
-        }, 5000); // Adjust the interval as needed
+  const webcamStyle = {
+    width: '100%',
+    height: '450px',
+    border: '3px solid #ccc',
+    borderRadius: '8px',
+    boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)',
+  };
 
-        return () => clearInterval(interval);
-    }, [capture]);
+  return (
+    <Flex flexDir={'column'} gap={2.5}>
+      <Flex flexDir={'row'} justify={'flex-end'}>
+          <Button isDisabled={!shouldCapture} disabled={!shouldCapture} onClick={capture}>
+            <img src={image.default.src} width={75} />
+          </Button>
+      </Flex>
+      <Webcam className='webcamStyle' style={{...webcamStyle, objectFit: 'cover'}} ref={webcamRef} screenshotFormat="image/jpeg" />
+    </Flex>
+  )
+}
 
-    return (
-        <Webcam ref={webcamRef} screenshotFormat="image/jpeg" />
-    );
-};
-
-export default WebcamCapture;
+export default WebcamCapture
--- src/pages/aifun.tsx
+++ src/pages/aifun.tsx
@@ -1,25 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import WebcamCapture from '../components/webcam'
-import { Box, Flex, Heading, Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import { Flex, Heading, Radio, RadioGroup, Stack } from '@chakra-ui/react'
 import Paragraph from '../components/paragraph'
+import { FaRegThumbsUp } from 'react-icons/fa6'
+import { TbDeviceRemote } from 'react-icons/tb'
 
 type VoiceTypes = 'Attenborough' | 'MovieTrailerGuy'
 
-const voices: { voiceType: VoiceTypes; voice: string, aiSetup: string }[] = [
-  { voiceType: 'Attenborough', voice: '70rymsvcj1TIa1tON70Z', aiSetup: "You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary."
-  + "Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!"},
-  { voiceType: 'MovieTrailerGuy', voice: 'W9Sme726LTRRST7a9TqU', aiSetup: "You are Don LaFontaine, the Movie Trailer Guy. Make a comical yet serious narration about the person in the image, make it dramatic and full of action. This is the coolest guy on earth."
-  + "Talk in a dramatic way, much like the movie trailer guy. A little bit up and a little bit down in the pitch of the voice." }
+const voices: { voiceType: VoiceTypes; voice: string; aiSetup: string }[] = [
+  {
+    voiceType: 'Attenborough',
+    voice: '70rymsvcj1TIa1tON70Z',
+    aiSetup:
+      'You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.' +
+      "Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!"
+  },
+  {
+    voiceType: 'MovieTrailerGuy',
+    voice: 'W9Sme726LTRRST7a9TqU',
+    aiSetup:
+      'You are Don LaFontaine, the Movie Trailer Guy. Make a comical yet serious narration about the person in the image, make it dramatic and full of action. This is the coolest guy or girl on earth.' +
+      'Talk in a dramatic way, much like the movie trailer guy. A little bit up and a little bit down in the pitch of the voice.'
+  }
 ]
 
 const AIFun: React.FC = () => {
   const [imageSrc, setImageSrc] = useState<string | null>(null)
   const [text, setText] = useState('')
-  const [voice, setVoice] = useState<{voiceType: VoiceTypes, voice: string, aiSetup: string}>(
-    {voiceType: "Attenborough", voice: "70rymsvcj1TIa1tON70Z", aiSetup: "You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary."
-    + "Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!"}
-  )
+  const [countdown, setCountdown] = useState<number | null>(null);
+  const [voice, setVoice] = useState<{
+    voiceType: VoiceTypes
+    voice: string
+    aiSetup: string
+  }>({
+    voiceType: 'Attenborough',
+    voice: '70rymsvcj1TIa1tON70Z',
+    aiSetup:
+      'You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.' +
+      "Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!"
+  })
   const [playingSound, setPlayingSound] = useState(false)
 
   const handleImageCapture = (image: string | null) => {
@@ -27,7 +46,8 @@ const AIFun: React.FC = () => {
     handleAudioStart()
     setImageSrc(image)
     if (image) {
-      sendImageToOpenAI(image)
+      setCountdown(5);
+      setTimeout(() => sendImageToOpenAI(image), 5000)
     } else {
       handleAudioEnd()
     }
@@ -108,6 +128,23 @@ const AIFun: React.FC = () => {
     getTextAndPlayIt()
   }, [text])
 
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+    if (countdown !== null && countdown > 0) {
+      interval = setInterval(() => {
+        setCountdown(currentCountdown => currentCountdown! - 1);
+      }, 1000);
+    } else if (interval) {
+      clearInterval(interval);
+    }
+
+    // Clear interval on cleanup
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [countdown]);
+
+
   const sendImageToOpenAI = async (base64Image: string) => {
     if (base64Image === '') return
     let finalString = ''
@@ -137,24 +174,16 @@ const AIFun: React.FC = () => {
     }
     setText(finalString)
   }
-  
+
   const handleSetVoice = (voiceType: VoiceTypes) => {
-    const theVoice = voices.find(e => e.voiceType === voiceType);
+    const theVoice = voices.find(e => e.voiceType === voiceType)
     if (theVoice) {
-      setVoice(theVoice);
+      setVoice(theVoice)
     }
-  };
-  
+  }
 
   return (
     <Flex align={'center'} flexDir={'column'} gap={10} mt={10}>
-      <Paragraph as={'div'} style={{ maxWidth: 500 }}>
-        {voice.voiceType} vil våke over webcamet ditt om
-        noen få sekunder, vent til indikasjonen dukker opp.
-      </Paragraph>
-      <Paragraph>
-        "Strike a pose" f.eks tommelen opp, eller vis et objekt f.eks en fjernkontroll. Ingenting blir lagret.
-      </Paragraph>
       <RadioGroup defaultValue="Attenborough" onChange={handleSetVoice}>
         <Stack spacing={5} direction="row">
           {voices.map((v, id) => (
@@ -164,9 +193,21 @@ const AIFun: React.FC = () => {
           ))}
         </Stack>
       </RadioGroup>
-      <Heading>
-        {playingSound ? `${voice.voiceType} ser på deg.` : 'Venter...'}
-      </Heading>
+      <Flex flexDir="column" alignItems={'center'} gap={2}>
+        <Heading>
+        {playingSound ? 
+          `${voice.voiceType} ser på deg.. ${countdown || ''}` : 
+          'Ready, steady, go...'}
+        </Heading>
+        <Flex alignItems={'center'} gap={2}>
+          <Paragraph style={{ verticalAlign: 'center' }}>
+            "Strike a pose" f.eks
+          </Paragraph>
+          <FaRegThumbsUp color="green" size={30} />
+          <Paragraph>Eller vis et objekt f.eks</Paragraph>
+          <TbDeviceRemote color="green" size={30} />
+        </Flex>
+      </Flex>
       <WebcamCapture
         shouldCapture={!playingSound}
         onCapture={(image: string | null) => handleImageCapture(image)}
