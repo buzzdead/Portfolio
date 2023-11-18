0b8313b8b67218f879865dd735d53ea20e9d35a3
buzzdead <sigmundvolden@gmail.com>
2023-11-18 16:43:53 +0100

    switch camera

--- src/components/webcam.tsx
+++ src/components/webcam.tsx
@@ -1,6 +1,8 @@
 import { Button, Flex, Spinner } from '@chakra-ui/react'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam'
+import { PiCameraRotateLight } from 'react-icons/pi'
+
 const image = require('../../public/camerabutton.jpg')
 
 interface Props {
@@ -10,18 +12,25 @@ interface Props {
   captureStart: () => void
 }
 
-const WebcamCapture: React.FC<Props> = ({ onCapture, captureStart, shouldCapture, isLoading = false }) => {
+const WebcamCapture: React.FC<Props> = ({
+  onCapture,
+  captureStart,
+  shouldCapture,
+  isLoading = false
+}) => {
   const webcamRef = useRef<Webcam>(null)
   const [pictureUrl, setPictureUrl] = useState('')
-  const [errorMessage, setErrorMessage] = useState('') // New state for error message
+  const [useBackCamera, setUseBackCamera] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [hasMultipleCameras, setHasMultipleCameras] = useState(false)
 
-  const capture = () =>  {
+  const capture = () => {
     if (shouldCapture) {
       captureStart()
       setTimeout(() => {
         const image = webcamRef.current?.getScreenshot()
-      onCapture(image || null)
-      setPictureUrl(image || "")
+        onCapture(image || null)
+        setPictureUrl(image || '')
       }, 5000)
     }
   }
@@ -29,47 +38,83 @@ const WebcamCapture: React.FC<Props> = ({ onCapture, captureStart, shouldCapture
   const handleCameraError = (error: any) => {
     // Check if the error is due to user denying camera access
     console.log(error)
-    if (error.name === "NotAllowedError") {
-      setErrorMessage('Camera access was denied. Please allow camera access to use this feature.')
+    if (error.name === 'NotAllowedError') {
+      setErrorMessage(
+        'Camera access was denied. Please allow camera access to use this feature.'
+      )
     } else {
-      setErrorMessage('No camera found. Please ensure your camera is connected and enabled.')
+      setErrorMessage(
+        'No camera found. Please ensure your camera is connected and enabled.'
+      )
     }
   }
 
-  const webcamStyle = {
-    width: '100%',
-    height: '450px',
-    border: '3px solid #ccc',
-    borderRadius: '8px',
-    boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)',
+  useEffect(() => {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(devices => {
+        const videoInputs = devices.filter(
+          device => device.kind === 'videoinput'
+        )
+        setHasMultipleCameras(videoInputs.length > 1)
+      })
+      .catch(err => {
+        console.error('Error accessing media devices:', err)
+      })
+  }, [])
+
+  const videoConstraints = {
+    facingMode: useBackCamera ? { exact: 'environment' } : 'user'
   }
 
   return (
     <Flex flexDir={'column'} gap={2.5}>
-      <Flex flexDir={'row'} justify={'flex-end'}>
+      <Flex
+        flexDir={'row'}
+        justify={hasMultipleCameras ? 'space-between' : 'flex-end'}
+        pl={2}
+      >
+        {hasMultipleCameras && (
+          <Button
+            padding={2}
+            backgroundColor={'transparent'}
+            onClick={() => setUseBackCamera(!useBackCamera)}
+          >
+            <PiCameraRotateLight size={40} />
+          </Button>
+        )}
         <Button
           isDisabled={!shouldCapture}
           disabled={!shouldCapture}
           onClick={capture}
         >
-          {isLoading ? <Spinner />
-          :
-          <img src={image.default.src} width={75} />
-          }
+          {isLoading ? <Spinner /> : <img src={image.default.src} width={75} />}
         </Button>
       </Flex>
-      {shouldCapture ? 
-      <Webcam
-      onUserMediaError={handleCameraError}
-      style={{ width: '100%', height: '450px', borderRadius: '8px', boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)', objectFit: 'cover' }}
-      ref={webcamRef}
-      screenshotFormat="image/jpeg"
-    /> :
-    <div className="block">
-      <img src={pictureUrl} />
-      </div>
-      }
-      {errorMessage && <Flex justifyContent={'center'} color="red.500">{errorMessage}</Flex>}
+      {shouldCapture ? (
+        <Webcam
+          videoConstraints={videoConstraints}
+          onUserMediaError={handleCameraError}
+          style={{
+            width: '100%',
+            height: '450px',
+            borderRadius: '8px',
+            boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)',
+            objectFit: 'cover'
+          }}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+        />
+      ) : (
+        <div className="block">
+          <img src={pictureUrl} />
+        </div>
+      )}
+      {errorMessage && (
+        <Flex justifyContent={'center'} color="red.500">
+          {errorMessage}
+        </Flex>
+      )}
     </Flex>
   )
 }
