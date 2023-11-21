6c65d80e2071b24e4d6b202c24085dd2eb2d1872
buzzdead <sigmundvolden@gmail.com>
2023-11-21 15:07:36 +0100

    rearranged order

--- src/lib/voices.ts
+++ src/lib/voices.ts
@@ -1,10 +1,12 @@
-export type VoiceTypes = 'Seinfeld' | 'MovieTrailerGuy' | 'Attenborough'
+export type VoiceTypes = 'Attenborough' | 'MovieTrailerGuy' | 'Seinfeld'
 
 export const voices: { voiceType: VoiceTypes; voice: string; aiSetup: string }[] = [
   {
-    voiceType: "Seinfeld",
-    voice: 'EVZnbelNdigmTyNlxhZj',
-    aiSetup: 'You are Jerry Seinfeld. Tell a joke about the picture, observations in the picture and the person in the picture.'
+    voiceType: 'Attenborough',
+    voice: 'VBUa5s91v0aDpqtiEQrO',
+    aiSetup:
+      'You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.' +
+      "Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!"
   },
   {
     voiceType: 'MovieTrailerGuy',
@@ -14,10 +16,8 @@ export const voices: { voiceType: VoiceTypes; voice: string; aiSetup: string }[]
       'Talk in a dramatic way, much like the movie trailer guy, but skip asterisks (*). We are currently in christmas season. Focus on the details, make a big deal out of it.'
   },
   {
-    voiceType: 'Attenborough',
-    voice: 'VBUa5s91v0aDpqtiEQrO',
-    aiSetup:
-      'You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.' +
-      "Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!"
-  },
+    voiceType: "Seinfeld",
+    voice: 'EVZnbelNdigmTyNlxhZj',
+    aiSetup: 'You are Jerry Seinfeld. Tell a joke about the picture, observations in the picture and the person in the picture.'
+  }
 ]
