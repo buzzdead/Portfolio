95ebec3f6735d36caa916e2f3ac7f9f36a64e4fa
buzzdead <sigmundvolden@gmail.com>
2025-03-25 13:35:15 +0100

    asdf

--- src/components/Introduction/Intro.tsx
+++ src/components/Introduction/Intro.tsx
@@ -1,34 +1,14 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import {
-  ScrollControls,
-  Scroll,
-  useScroll,
-  Stars,
-  Environment,
-  useCursor
-} from '@react-three/drei'
-import { motion } from 'framer-motion'
-import { useRef, useState } from 'react'
-import { ArrowDown } from 'lucide-react'
+import { Canvas } from '@react-three/fiber'
+import { useRef } from 'react'
 import {
   GodRays,
   ChromaticAberration,
   EffectComposer
 } from '@react-three/postprocessing'
-import { Vector2 } from 'three'
 import Logo from '../Logo'
 import Particles from './Particles'
 import SkipButton from './SkipButon'
 import Sphere from './Sphere'
-
-const GradientBackground = () => (
-  <mesh scale={100}>
-    <planeGeometry args={[1, 1, 32, 32]} />
-    <meshBasicMaterial>
-      <canvasTexture attach="map" args={[createGradientCanvas(1024, 1024)]} />
-    </meshBasicMaterial>
-  </mesh>
-)
 
 function createGradientCanvas(w: number, h: number) {
   const canvas = document.createElement('canvas')
@@ -47,11 +27,6 @@ const Intro = () => {
     <Canvas>
     <EffectComposer>
               <GodRays sun={sphereRef.current} blur={true} samples={30} />
-      {
-        //@ts-ignore
-
-       
-      }
       <Sphere />
        <Logo />
        <Particles />
